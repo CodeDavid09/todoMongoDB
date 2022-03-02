ae9bbedee0c62942c7c7eee5b117ff88182c8551
David Gavino <ggavino2@uccs.edu>
2022-03-01 18:38:50 -0700

    modified todo.js

--- model/Todo.js
+++ model/Todo.js
@@ -2,6 +2,12 @@ const { Schema, model } = require('mongoose');
 
 // the Schema is very similar to the "class" that we were creating in sequelize
 const todoSchema = new Schema({
+    userId: {
+        // objectId type
+        type: Schema.Types.ObjectId,
+        // ref mean which collection does thie objectId referenced.
+        ref: 'User'
+    },
     text: {
         type: String,
         required: true,
